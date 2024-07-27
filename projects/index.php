e0d013c2fe86e0a8755d590c782f594e330c5597
diegot-code <torres.db04@gmail.com>
2024-07-26 21:17:11 -0400

    Preparing For Php

--- projects/index.php
+++ projects/index.php
@@ -1,9 +1,16 @@
+<?php
+
+$pageTitle = "Projects";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Projects</title>
+    <title><?=$pageTitle?></title>
   </head>
   <body>
     <h1>Display all Projects as Cards and links to them using github API</h1>
