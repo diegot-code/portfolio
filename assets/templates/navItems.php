b2cc6c1daf270259aa4b0ff85fa720576dc5c49b
diegot-code <torres.db04@gmail.com>
2024-08-06 20:34:06 -0400

    Navigation Array

---
+++ assets/templates/navItems.php
@@ -0,0 +1,19 @@
+<?php
+$navigation_items = [
+    [
+        "name" => "Home",
+        "path" => "index.php",
+    ],
+    [
+        "name" => "Projects",
+        "path" => "/projects",
+    ],
+    [
+        "name" => "About",
+        "path" => "/journey",
+    ],
+    [
+        "name" => "Connect",
+        "path" => "/connect",
+    ],
+];
