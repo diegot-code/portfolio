bc1132ad20d9b714afdf1c690d0de13910fd3d97
diegot-code <torres.db04@gmail.com>
2024-08-07 15:59:51 -0400

    Added Style pair inside nav array

--- assets/templates/navItems.php
+++ assets/templates/navItems.php
@@ -3,17 +3,25 @@ $navigation_items = [
     [
         "name" => "Home",
         "path" => "/",
+        "a-s" => "nav-item",
+        "li-s" => "",
     ],
     [
         "name" => "Projects",
         "path" => "/projects",
+        "a-s" => "nav-item",
+        "li-s" => "",
     ],
     [
         "name" => "About",
         "path" => "/journey",
+        "a-s" => "nav-item",
+        "li-s" => "",
     ],
     [
         "name" => "Connect",
         "path" => "/connect",
+        "a-s" => "nav-item",
+        "li-s" => "",
     ],
 ];
